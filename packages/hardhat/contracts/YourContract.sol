b6d50b1d3feca26237a35dc442cd376445fd17dc
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2022-02-03 14:00:06 -0800

    2/3: Commit first smart contract
    Signed-off-by: Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,27 +1,76 @@
 pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
-import "hardhat/console.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol"; 
-// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
-
 contract YourContract {
+    struct tweet {
+        string message;
+        address owner;
+        uint256 likes_count;
+        mapping(address => bool) liker;
+        mapping(uint256 => address) idToLiker;
+    }
+    struct user {
+        string name;
+        uint256 tweets_count;
+        uint256 followers_count;
+        uint256[] tweets;
+        mapping(address => bool) followers;
+        mapping(uint256 => address) idToFollower;
+    }
+    mapping(address => user) public addrToUser;
+    mapping(uint256 => tweet) public gTweets;
+    uint256 public gTweetsCount;
 
-  // event SetPurpose(address sender, string purpose);
+    function fTweet(string calldata _message) external {
+        tweet storage t = gTweets[gTweetsCount];
+        t.message = _message;
+        t.owner = msg.sender;
+        user storage u = addrToUser[msg.sender];
+        u.tweets_count += 1;
+        u.tweets.push(gTweetsCount);
+        gTweetsCount += 1;
+    }
 
-  string public purpose = "Building Unstoppable Apps!!!";
+    function fCreateUser(string memory _name) public {
+        addrToUser[msg.sender].name = _name;
+    }
 
-  constructor() payable {
-    // what should we do on deploy?
-  }
+    function follow(address _dest) external {
+        user storage u = addrToUser[_dest];
+        require(u.followers[msg.sender] == false, "Already Following");
+        u.followers[msg.sender] = true;
+        u.idToFollower[u.followers_count] = msg.sender;
+        u.followers_count += 1;
+    }
 
-  function setPurpose(string memory newPurpose) public {
-      purpose = newPurpose;
-      console.log(msg.sender,"set purpose to",purpose);
-      // emit SetPurpose(msg.sender, purpose);
-  }
+    function flike(uint256 _index) external {
+        tweet storage t = gTweets[_index];
+        require(t.liker[msg.sender] == false, "Already Liked");
+        t.liker[msg.sender] = true;
+        t.idToLiker[t.likes_count] = msg.sender;
+        t.likes_count += 1;
+    }
 
-  // to support receiving ETH by default
-  receive() external payable {}
-  fallback() external payable {}
+    function getFollowers(address _addr) external view returns(address[] memory) {
+        user storage u = addrToUser[_addr];
+        uint256 len = u.followers_count;
+        address[] memory a = new address[](len);
+        for(uint256 i=0; i < len; i++) {
+            a[i] = u.idToFollower[i];
+        }
+        return a;
+    }
+
+    function getLikes(uint256 _index) external view returns(address[] memory) {
+        require(_index < gTweetsCount, "invalid Tweet id");
+        tweet storage t = gTweets[_index];
+        uint256 len = t.likes_count;
+        address[] memory a = new address[](len);
+        for(uint256 i=0; i < len; i++) {
+            a[i] = t.idToLiker[i];
+        }
+        return a;
+    }
 }
+
+
