7e213c56575a91e199322094a48e6d019100d694
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2022-02-27 23:27:13 -0800

    2/27: Add interface hash in comment
    Signed-off-by: Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>

--- packages/hardhat/contracts/ERC165.sol
+++ packages/hardhat/contracts/ERC165.sol
@@ -5,6 +5,7 @@ contract ERC165 {
     mapping (bytes4 => bool) internal supportedInterfaces;
 
     constructor(){
+        //0x01ffc9a7
         supportedInterfaces[bytes4(keccak256("supportsInterface(bytes4)"))] = true;
     }
     
